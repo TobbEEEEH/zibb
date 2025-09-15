17eb1787b48c0e657f98025835f7c24246f30eda
Tobias Östh <tobias.osth@gmail.com>
2025-09-15 22:04:59 +0200

    fix: made some visitor statistics

--- storage/fakedb.go
+++ storage/fakedb.go
@@ -1,6 +1,34 @@
 // Package storage serves a fake DB right now
 package storage
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+)
 
-var Users sync.Map
+var (
+	Users          sync.Map
+	requestCounter = make(map[string]int)
+)
+
+func UpdateRequestCounter(key string) {
+	if val, ok := requestCounter[key]; ok {
+		val++
+		requestCounter[key] = val
+	} else {
+		requestCounter[key] = 1
+	}
+
+	fileContent := ""
+	for key, val := range requestCounter {
+		fileContent += fmt.Sprintf("%s: %d\n", key, val)
+	}
+
+	file, err := os.Create("storage/visitors.txt")
+	if err != nil {
+		return
+	}
+
+	file.Write([]byte(fileContent))
+}
